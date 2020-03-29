24e33fae7367143b58ae3c96f4a43b367661893c
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-03-29 16:26:52 -0700

    Implement top10list via delegation

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -2,20 +2,24 @@
 #include <string>
 #include "TopTenList.h"
 #include "Hyperlink.h"
+#include <vector>
 
 using std::string;
 
 TopTenList::TopTenList()
 {
-
+  _list.resize(10);	
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+  if (index < 0 || index >= _list.size()) {
+    return;
+  }
+  _list[index] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+  return _list.at(index);
 }
