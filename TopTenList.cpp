0ecf05d4b51c6bce7496f6f89a36ccfc96eaac37
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-03-31 15:09:32 -0700

    Implement IDirectionable interface

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -3,23 +3,37 @@
 #include "TopTenList.h"
 #include "Hyperlink.h"
 #include <vector>
+#include <iostream>
 
 using std::string;
+using std::cout;
 
 TopTenList::TopTenList()
 {
-  _list.resize(10);	
+    _list.resize(10);	
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-  if (index < 0 || index >= _list.size()) {
-    return;
-  }
-  _list[index] = link;
+    if (index < 0 || index >= _list.size()) {
+      return;
+    }
+    _list[index] = link;
+}
+
+void TopTenList::display_forward() {
+    for (int i = 0; i < 10; i++) {
+      cout << _list[i].text << "\n";
+    }
+}
+
+void TopTenList::display_backward() {
+    for (int i = 9; i >= 0; i--) {
+      cout << _list[i].text << "\n";
+    }
 }
 
 Hyperlink TopTenList::get(int index)
 {
-  return _list.at(index);
+    return _list.at(index);
 }
